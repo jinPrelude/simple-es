7ff1cec87fc3c02dc797d665ba25f6ed549f9da7
euijin <jin.Prelude@gmail.com>
2021-01-17 22:54:41 +0900

    Enhance EatAppleModel

--- networks/vanila_neural_network.py
+++ networks/vanila_neural_network.py
@@ -29,27 +29,34 @@ class DQNModel(nn.Module):
 
 
 class EatAppleModel(nn.Module):
-    def __init__(self, in_channels=1, num_actions=4):
+    def __init__(self, in_channels=1, num_action=4):
         super(EatAppleModel, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=1)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(32, 128)
-        self.gru = nn.GRU(128, 64)
-        self.fc2 = nn.Linear(64, num_actions)
+        self.num_action = num_action
+        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(16, 16, kernel_size=3, stride=1)
+        self.fc1 = nn.Linear(16, 32)
+        self.gru = nn.GRU(32, 32 + num_action)
+        self.fc2 = nn.Linear(32 + num_action, num_action)
+        self.last_action_onehot = torch.zeros((1, 1, self.num_action))
 
     def forward(self, x, h):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = x.view(x.size(0), 1, -1)
         x = F.relu(self.fc1(x))
+
         x, h = self.gru(x, h)
         x = F.relu(x)
         x = self.fc2(x)
         x = F.softmax(x.squeeze(), dim=0)
+
+        action = torch.argmax(x)
+        self.last_action_onehot = torch.zeros((1, 1, self.num_action))
+        self.last_action_onehot[0, 0, action] = 1
         return x, h
 
     def init_hidden(self):
-        return torch.zeros([1, 1, 64], dtype=torch.float)
+        return torch.zeros([1, 1, 32 + self.num_action], dtype=torch.float)
 
     def init_weights(self, mu=0, sigma=1):
         for param in self.parameters():
