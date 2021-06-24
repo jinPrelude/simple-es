c87d32f94f47a130d81b94475475ce6d1620487d
Euijin Jeong <euijin.jeong@medipixel.io>
2021-06-24 16:40:04 +0900

    Replace ray to mp

--- run_es.py
+++ run_es.py
@@ -3,7 +3,6 @@ import random
 
 import hydra
 import numpy as np
-import ray
 import torch
 from hydra.utils import instantiate
 from omegaconf import DictConfig, OmegaConf
@@ -17,14 +16,13 @@ def set_seed(seed):
     random.seed(seed)
 
 
-@hydra.main(config_path="conf", config_name="bipedalwalker_config")
+@hydra.main(config_path="conf", config_name="lunar_lander_config")
 def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
     network = instantiate(cfg.network)
     offspring_strategy = instantiate(cfg.offspring_strategy)
     env = instantiate(cfg.env)
-    ray.init()
-    ls = ESLoop.remote(
+    ls = ESLoop(
         offspring_strategy,
         env,
         network,
@@ -33,17 +31,7 @@ def main(cfg: DictConfig):
         cfg.eval_ep_num,
         cfg.log,
     )
-    ray.get(ls.run.remote())
-    # ls = ESLoop(
-    #     offspring_strategy,
-    #     env,
-    #     network,
-    #     cfg.generation_num,
-    #     cfg.cpu_num,
-    #     cfg.eval_ep_num,
-    #     cfg.log,
-    # )
-    # ls.run()
+    ls.run()
 
 
 if __name__ == "__main__":
