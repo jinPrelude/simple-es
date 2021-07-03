22f443def35777c1003a4c0d8a28b8763ac8ad80
euijin <jin.Prelude@gmail.com>
2021-07-03 14:54:12 +0900

    Add pomdp lunarlander

--- envs/gym_wrapper.py
+++ envs/gym_wrapper.py
@@ -5,8 +5,13 @@ gym.logger.set_level(40)
 
 
 class GymWrapper:
-    def __init__(self, name, max_step=None):
+    def __init__(self, name, max_step=None, pomdp=False):
         self.env = gym.make(name)
+        if "LunarLander" in name and pomdp:
+            print("POMDP LunarLander")
+            self.env = LunarLanderPOMDP(self.env)
+        elif pomdp and "LunarLander" not in name:
+            raise AssertionError(f"{name} doesn't support POMDP.")
         self.max_step = max_step
         self.curr_step = 0
         self.name = name
@@ -43,3 +48,14 @@ class GymWrapper:
 
     def close(self):
         self.env.close()
+
+class LunarLanderPOMDP(gym.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+
+    def observation(self, obs):
+        # modify obs
+        obs[2] = 0
+        obs[3] = 0
+        obs[5] = 0
+        return obs
