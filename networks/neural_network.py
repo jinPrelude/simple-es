7d3f71a08c0568565acf59df4482f538238cc12c
euijin <jin.Prelude@gmail.com>
2021-07-04 13:54:11 +0900

    Change cppn output

--- networks/neural_network.py
+++ networks/neural_network.py
@@ -42,6 +42,7 @@ class GymEnvModel(BaseNetwork):
         for param in self.parameters():
             nn.init.normal_(param, mu, sigma)
 
+
 class IndirectEncoding(BaseNetwork):
     def __init__(self, num_state=8, num_action=4, discrete_action=True):
         super(IndirectEncoding, self).__init__()
@@ -67,21 +68,25 @@ class IndirectEncoding(BaseNetwork):
 
     def cppn_forward(self, x):
         x = torch.tanh(self.cppn1(x))
-        x = torch.tanh(self.cppn2(x))*3.
+        x = self.cppn2(x)
         x = x.detach()
         return x
 
     def reset(self):
-        l1 = np.array([[i/(self.num_state*2), 0.25] for i in range(1, self.num_state+1)])
-        l2 = np.array([[i/(32*2), 0.5] for i in range(1, 33)])
-        l3 = np.array([[i/(self.num_action*2), 0.75] for i in range(1, self.num_action+1)])
+        l1 = np.array(
+            [[i / (self.num_state * 2), 0.25] for i in range(1, self.num_state + 1)]
+        )
+        l2 = np.array([[i / (32 * 2), 0.5] for i in range(1, 33)])
+        l3 = np.array(
+            [[i / (self.num_action * 2), 0.75] for i in range(1, self.num_action + 1)]
+        )
         fc1 = torch.zeros(32, self.num_state).float()
         fc2 = torch.zeros(self.num_action, 32).float()
         for idx_i, i in enumerate(l2):
             for idx_j, j in enumerate(l1):
                 cppn_input = torch.tensor(np.concatenate((j, i))).float()
                 fc1[idx_i][idx_j] = self.cppn_forward(cppn_input)
-        
+
         for idx_i, i in enumerate(l3):
             for idx_j, j in enumerate(l2):
                 cppn_input = torch.tensor(np.concatenate((j, i))).float()
